f2ed0c5873e0672c3be9545c7ee6d8236559afc1
paulfruitful <75788347+paulfruitful@users.noreply.github.com>
2021-12-31 00:51:49 +0100

    Update studentportal.php

--- studentportal.php
+++ studentportal.php
@@ -23,7 +23,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#">SheddyBen Lib-BookSys</a>
+				<a class="navbar-brand" href="#"> Lib-Sys</a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example">
@@ -51,22 +51,10 @@
 					<div class="carousel" data-flickity='{ "autoPlay": true }'; >
 
 						  <div class="carousel-cell" auto-play >
-						  	<img src="images/header-bg.jpg">
-						  </div>
-						  <div class="carousel-cell" auto-play>
-						  	<img src="images/header-bg.jpg">
-
-						  </div>
-						  <div class="carousel-cell" auto-play>
-						  	 <img src="images/header-bg.jpg">
+						  	<img src="library-images/get.jpg">
 						  </div>
 						  
-						  <div class="carousel-cell" auto-play >
-						  	<img src="images/header-bg.jpg">
-						  </div>
-						   <div class="carousel-cell" auto-play>
-						  	<img src="images/header-bg.jpg">
-						  </div>
+					
 
 					</div>
 
@@ -80,38 +68,21 @@
 	  		<div class="row">
 	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
 		  			<div class="page-header col-lg-offset-1">
-		  				<h2>Welcome to our portal</h2>
+		  				<h2 style="color:black;">Welcome to our portal</h2>
 		  			</div>
-	  				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	  				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	  				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	  				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	  				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	  				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  		Welcome to Paul's Library System;
+			  Hope you will have a wonderful user experience;
+			  If you have any problem 
 
-	  				<a href="#"><p class="slide2"><button class="btn btn-success">Sign Up</button></p></a>
+	  				<a href="tel:+2348114483062"><p class="slide2"><button class="btn btn-success">Contact Me</button></p></a>
 	  			</div>
 	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
 		  			<div class="page-header col-lg-offset-1">
-		  				<h2>24/7 Operational Support</h2>
+		  				<h2 style="color:black;">24/7 Operational Support</h2>
 		  			</div>
-	  				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	  				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	  				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	  				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	  				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	  				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	  			Our Servers Are Up And Running 24/7 To Provide You Maximum Satisfaction while Using Our System
 	  			</div>
-	  			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 column">
-	  				<div class="page-header col-lg-offset-1">
-	  				<h2>Why Us?</h2>
-	  			</div>
-	  				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	  				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	  				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	  				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	  				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	  				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
 	  			</div>
 	  		</div>
 		</div>
@@ -121,40 +92,38 @@
 				<div class="row">
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
 					<a href="#" class="thumbnail">
-						<img src="images/thumbnail.jpg">
+						<img src="library-images/get.jpg">
 					</a>
 				</div>
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
 					<a href="#" class="thumbnail">
-						<img src="images/thumbnail.jpg">
+						<img src="library-images/nat.jpg">
 					</a>
 				</div>
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
 					<a href="#" class="thumbnail">
-						<img src="images/thumbnail.jpg">
+						<img src="library-images/get.jpg">
 					</a>
 				</div>
 				<div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
 					<a href="#" class="thumbnail">
-						<img src="images/thumbnail.jpg">
+						<img src="library-images/nat.jpg">
 					</a>
 				</div>
+				<div class="container_fluid">
+			
+			<p class="page-header center-block">
+				<a href="tel:+2348114483062"><button class="btn btn-primary">Contact Us</button></a>
+				<a href="mailto:fruitful2007@outlook.com"><button class="btn btn-info">About Us</button></a>
+
+			</p>
+		</div>
+
 			</div>
 			</div>
 			
 		</div>
-		<div class="container_fluid">
-			<p class="page-header center-block">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				
-			</p>
-			<p class="page-header center-block">
-				<a href="#"><button class="btn btn-primary">Contact Us</button></a>
-				<a href="#"><button class="btn btn-info">About Us</button></a>
-
-			</p>
-		</div>
-
+		
 
 <script type="text/javascript" src="js/jquery.js"></script>
 <script type="text/javascript" src="js/bootstrap.js"></script>
